42b49e08983a5012f926b3d63eb59f71b3f55893
Alex <yjaelex@163.com>
2016-09-17 22:33:47 +0800

    Add bs4 lib.

--- savepic/savepic.py
+++ savepic/savepic.py
@@ -4,6 +4,7 @@
 import urllib.request
 import urllib.error
 from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 
 url = 'http://www.python.org/'
 try:
@@ -13,7 +14,11 @@ try:
 except urllib.error.HTTPError as e:
     print("Error Code:", e.code)
 except urllib.error.URLError as e:
-    print("Error Reason:", e.reason) 
+    print("Error Reason:", e.reason)
+
+soup = BeautifulSoup(mainHTML, 'html.parser')
+print(soup.prettify())
+
 
 
 
